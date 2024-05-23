4f21eab22d9d6113a65d8034cffd90982da6b66f
Gerson Alves <gersonalvesdev@gmail.com>
2024-05-23 12:05:52 -0300

    add check if command exists

--- src/Compress.php
+++ src/Compress.php
@@ -2,9 +2,11 @@
 
 namespace NexacodeTech\Compress;
 
+use Exception;
 use NexacodeTech\Compress\Enums\CompressTypeEnum;
 use NexacodeTech\Compress\Enums\OutputTypeEnum;
 use NexacodeTech\Compress\Enums\QualityEnum;
+use Symfony\Component\Process\Process;
 
 class Compress
 {
@@ -18,10 +20,11 @@ class Compress
     {
         $this->pdf = new CompressPDF();
         $this->image = new CompressImage();
+        $this->checkDependencies();
     }
 
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     public function compress(string $file, OutputTypeEnum $outputType, $output = ''): string
     {
@@ -40,5 +43,35 @@ class Compress
     {
         $this->quality = $quality;
         return $this;
+    }
+
+    private function commandExists($command): bool
+    {
+        $process = new Process(['which', $command]);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            return false;
+        }
+
+        return !empty(trim($process->getOutput()));
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function checkDependencies(): void
+    {
+        if ($this->fileType == CompressTypeEnum::PDF) {
+            if (!$this->commandExists('gs')) {
+                throw new Exception('Ghostscript is required to compress PDF files');
+            }
+        }
+
+        if ($this->fileType == CompressTypeEnum::IMAGE) {
+            if (!$this->commandExists('convert')) {
+                throw new Exception('ImageMagick is required to compress image files');
+            }
+        }
     }
 }
